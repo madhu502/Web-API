203c4f2f5fa9b5af8a9b082648da592d2be2387b
madhu502 <chaudharymadhu502@gmail.com>
2024-04-24 20:23:21 +0545

    Hashing done in register

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,4 +1,5 @@
 const userModel = require("../models/userModel");
+const bcrypt = require('bcrypt')
 
 const createUser = async (req, res) => {
   // 1. Check incoming data
@@ -28,27 +29,34 @@ const createUser = async (req, res) => {
         message: "User Already Exists!",
       });
     }
+
+    // Hashing /Encryption of the password
+    const randomSalt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, randomSalt)
+
+
+
+
     //5.1.1. stop the process
     //5.2 if user is new :
     const newUser = new userModel({
-        //Database fields : Client's value
-        firstName : firstName,
-        lastName : lastName,
-        email :email,
-        password : password
-    })
+      //Database fields : Client's value
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: hashedPassword,
+    });
     // 5.2.1  Hash the password
     //5.2.2 save to the database
-    await newUser.save()
+    await newUser.save();
 
     //5.2.3 send confirmation response
     res.json({
-        "success ": true,
-        "message" : "User created successfully"
-    })
-
+      "success ": true,
+      message: "User created successfully",
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
     res.json({
       success: false,
       message: "Internal server error !",
@@ -63,9 +71,58 @@ const createUser = async (req, res) => {
 // 4. Error handling( Try Catch)
 // 5. verification of user data in database:
 
-//
+const loginUser = async (req, res) => {
+  // 2. Destructure the incoming data
+  const { email, password } = req.body;
+
+  // 3. Validation of data (if empty, stop the process and send response)
+  if (!email || !password) {
+    return res.json({
+      success: false,
+      message: "Please enter both email and password!",
+    });
+  }
+
+  // 4. Error handling (Try Catch)
+  try {
+    // 5. Verification of user data in the database
+    const user = await userModel.findOne({ email: email });
+
+    // 5.1. Check if user exists
+    if (!user) {
+      return res.json({
+        success: false,
+        message: "User not found!",
+      });
+    }
+    // 5.2. Compare passwords
+    if (password !== user.password) {
+      return res.json({
+        success: false,
+        message: "Incorrect password!",
+      });
+    }
+    // 5.3. If user found and password matched, send success response
+    res.json({
+      success: true,
+      message: "Login successful!",
+      user: {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: "Internal server error!",
+    });
+  }
+};
 
 // Exporting
 module.exports = {
   createUser,
+  loginUser,
 };
